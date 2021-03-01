6d5de198fc8c0191e27573b86908ed1c554c715f
Zihan Weng <wzh.k@outlook.com>
2021-03-01 19:34:18 +0800

    refactored
    refactored
    
    refactored
    
    refactored
    
    refactored

---
+++ README.md
@@ -0,0 +1,15 @@
+# NEEPU Calendar
+
+## 功能
+
+将课程表导出为 iCal 日历文件
+
+## 使用方法
+
+pip 安装 requests, ics 后修改 main.py 中 userid 及 password 后运行即可
+
+## TODO
+
+- [ ] 异常处理 
+- [ ] 代码注释
+- [ ] CLI
--- main.py
+++ main.py
@@ -1,103 +1,72 @@
-import datetime
-from enum import Enum
+import re
+from datetime import datetime, timedelta
 
-import pandas as pd
-from bs4 import BeautifulSoup
-from ics import Event, Calendar
+from ics import Calendar, Event
+from ics.alarm import AudioAlarm
 
-
-class WeekDays(Enum):
-    Monday = '星期一'
-    Tuesday = '星期二'
-    Wednesday = '星期三'
-    Thursday = '星期四'
-    Friday = '星期五'
-    Saturday = '星期六'
-    Sunday = '星期日'
+from scrapper import get_table, get_session
 
 
-Days = {'星期一': 1, '星期二': 2, '星期三': 3, '星期四': 4, '星期五': 5, '星期六': 6, '星期日': 7, }
-
-
-def week_cleanup(week_range):
-    week_range = week_range.replace('周', '')
-    if week_range.find('-') != -1:
-        week_range = week_range.split('-')
-        ret_val = [x for x in range(int(week_range[0]), int(week_range[1]) + 1)]
-    else:
-        ret_val = [int(x) for x in week_range.split(',')]
-
-    return ret_val
-
-
-def neepu_calender_cleanup(html_input):
-    criculums = []
-    with open(html_input, 'r', encoding="utf-8") as source:
-        html_table = BeautifulSoup(source, features='lxml').table
-    table = pd.read_html(str(html_table).replace('<br/>', '|'))[0]
-    # print(html_table)
-
-    for ele in WeekDays:
-        current_day = table[[ele.value]]
-        # print(current_day)
-        for block in current_day.iterrows():
-            event = [block[0]]
-            event += str(block[1]).split(' ')
-            event = [x for x in event if x != '']
-            event[2] = str(event[2]).replace('\nName:', '')
-            if str(event[2]) == 'NaN':
-                continue
-
-            pos = event[0]
-            day = Days[str(event[1])]
-            info = [x for x in str(event[2]).split('|') if x != '']
-            info[0] = week_cleanup(info[0])
-
-            row = [pos, day]
-            row += info
-            criculums.append(row)
-    return criculums
-
-
-def curriculum_to_events(day, pos, name, location, info, week, starting_day):
-    events = []
-    time_table = {
-        0: ['00:00:00', '01:35:00'],
-        1: ['02:00:00', '03:35:00'],
-        2: ['05:30:00', '07:05:00'],
-        3: ['07:30:00', '09:05:00'],
-        4: ['10:30:00', '12:05:00'],
+def build_calender(time_table, alarm=15):
+    routine = {
+        1 : ['00:00:00', '00:45:00'],
+        2 : ['00:50:00', '01:35:00'],
+        3 : ['02:00:00', '02:45:00'],
+        4 : ['02:50:00', '03:35:00'],
+        5 : ['05:30:00', '06:15:00'],
+        6 : ['06:20:00', '07:05:00'],
+        7 : ['07:30:00', '08:15:00'],
+        8 : ['08:20:00', '09:05:00'],
+        9 : ['10:30:00', '11:15:00'],
+        10: ['11:20:00', '12:05:00'],
     }
-    # print(location)
-    while starting_day.weekday() != 0:
-        starting_day += datetime.timedelta(days=1)
+    days = {'一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '日': 7}
 
-    for wk in week:
-        current_day = \
-            starting_day + datetime.timedelta(weeks=wk - 1) \
-            + datetime.timedelta(days=day - 1)
-        e = Event()
-        e.name = name
-        e.begin = current_day.isoformat() + ' ' + time_table[pos][0]
-        e.end = current_day.isoformat() + ' ' + time_table[pos][1]
-        e.description = info
-        e.location = location
-        events.append(e)
+    calendar = Calendar()
+    time_detail_re = re.compile(u'星期([\u4e00-\u9fa5])(\d+)-(\d+)')
+    duration_re = re.compile(r'(\d+)-(\d+)')
 
-    return events
+    term_start = datetime.strptime(time_table['开始时间'], '%Y-%m-%d').date()
+    term_end = datetime.strptime(time_table['结束时间'], '%Y-%m-%d').date()
+
+    for lesson in time_table['课表信息']:
+        lesson_duration = lesson['学时分布']
+        start_week, end_week = duration_re.match(lesson_duration).groups()
+
+        for time_detail in lesson['节次信息']:
+            weekday, class_start, class_end = time_detail_re.match(time_detail['节次']).groups()
+            for week in range(int(start_week), int(end_week)):
+                if time_detail['时间类型'] == '单周':
+                    if week % 2 != 1:
+                        continue
+                if time_detail['时间类型'] == '双周':
+                    if week % 2 != 0:
+                        continue
+
+                event = Event()
+                event.name, event.organizer, event.location \
+                    = lesson['课程名称'], lesson['教师姓名'], time_detail['教室']
+                a = AudioAlarm(trigger=timedelta(minutes=alarm))
+                event.alarms.append(a)
+                base_day = term_start + timedelta(weeks=week - 1, days=days[weekday] - 1)
+                class_start_time = datetime.strptime(routine[int(class_start)][0], '%H:%M:%S').time()
+                class_end_time = datetime.strptime(routine[int(class_end)][1], '%H:%M:%S').time()
+
+                event.begin = datetime.combine(base_day, class_start_time)
+                event.end = datetime.combine(base_day, class_end_time)
+                calendar.events.add(event)
+
+    return calendar
 
 
-if __name__ == "__main__":
-    html_file = "学期课表.html"
-    starting_day = datetime.date(2020, 8, 22)
-    filename = "gsx.ics"
-    curriculums = neepu_calender_cleanup(html_file)
-    classes = []
-    for lesson in curriculums:
-        classes += curriculum_to_events(lesson[1], lesson[0], lesson[3], lesson[5], lesson[4], lesson[2], starting_day)
+if __name__ == '__main__':
+    # Change to your own userid and password
+    userid = "2018309020226"
+    password = ""
 
-    cal = Calendar()
-    for cla in classes:
-        cal.events.add(cla)
-    with open(filename, 'w', encoding='utf-8') as my_file:
-        my_file.writelines(cal)
+    session_id = get_session(userid, password)
+    table = get_table(session_id, "20202")
+
+    with open('my.ics', 'wt', encoding='utf-8') as f:
+        f.write(str(build_calender(table)))
+    # print(str(build_calender(table)))
